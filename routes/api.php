ee2d4988d6a3366f2f2523d51ea0f220e3b9c699
kijenny <kiranajenny46@gmail.com>
2021-11-22 07:37:28 +0700

    membuat REST API CRUD

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ProgramController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -30,6 +32,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     Route::get('/profile', function(Request $request) {
     return auth()->user();
     });
+
+    Route::resource('programs', ProgramController::class);
     
     // API route for logout user
     Route::post('/logout', [AuthController::class, 'logout']);
